b67330416878e419a48f77d22e2cdff837a2d3cc
Maxim <creativesun@yandex.ru>
2021-12-05 23:45:09 +0300

    add task 2

--- main.js
+++ main.js
@@ -28,3 +28,20 @@ class Elem {
 }
 
 const elem = new Elem(".q");
+
+/* Task 2 */
+
+class User {
+  constructor(name, surname) {
+    this.name = name;
+    this.surname = surname;
+    this.getFullName = this.name + " " + this.surname;
+  }
+}
+class Student extends User {
+  constructor(name, surname, year) {
+    super(name, surname);
+    this.year = year;
+    this.getCourse = new Date().getFullYear() - this.year;
+  }
+}
